629ae131f4a7cb3a13378fb467d6dc4ae5c0af22
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-19 23:46:20 +0530

    ConditionalIf.java file has been added

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num2 = 87;
+        int num3 = 95;
+        int num4 = 100;
+        boolean value = true;
+        if (value){
+            System.out.println("num1 is greater than num2");
+            if (value) {
+                System.out.println("num2 is greater than num3");
+                if (value) {
+                    System.out.println("num3 is greater than num4");
+                }
+                else{
+                    System.out.println("num1 is less than num2");
+                }
+                int num1 = 33;
+            }
+
+        }
+    }
+}
